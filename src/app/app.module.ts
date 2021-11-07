162b135817852e51b1267d36da8fe7d45a17fa96
mglatki <mglatki@gmail.com>
2021-11-07 15:12:22 +0100

    Reducers added to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
